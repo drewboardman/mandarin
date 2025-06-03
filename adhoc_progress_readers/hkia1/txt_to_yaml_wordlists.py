ec1c0dbb5f4bb611abc783efcdbbbc8584208841
Drew Boardman <drew.e.boardman@gmail.com>
2025-06-03 18:57:37 -0400

    more reader stuff

--- adhoc_progress_readers/hkia1/txt_to_yaml_wordlists.py
+++ adhoc_progress_readers/hkia1/txt_to_yaml_wordlists.py
@@ -34,9 +34,18 @@ def main():
     new_words = parse_txt_words(args.txt_file)
     known_words = get_known_words(db_path, new_words)
 
+    # Split new_words into 3 evenly sized sets
+    n = len(new_words)
+    set_size = (n + 2) // 3
+    set1 = new_words[:set_size]
+    set2 = new_words[set_size:2*set_size]
+    set3 = new_words[2*set_size:]
+
     with open(args.output_yaml, 'w', encoding='utf-8') as out:
-        out.write('new_words: ' + ', '.join(new_words) + '\n')
         out.write('known_words: ' + ', '.join(known_words) + '\n')
+        out.write('new_words_set1: ' + ', '.join(set1) + '\n')
+        out.write('new_words_set2: ' + ', '.join(set2) + '\n')
+        out.write('new_words_set3: ' + ', '.join(set3) + '\n')
 
 if __name__ == '__main__':
     main()
